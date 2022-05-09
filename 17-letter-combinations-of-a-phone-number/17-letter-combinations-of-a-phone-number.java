b32fe96ef6bd964cd30aa0c4ac635575ed356db7
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-05-09 20:55:44 +0530

    Time: 8 ms (26.54%), Space: 43.2 MB (14.81%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,36 @@
 class Solution {
     
-    static String[] letters = {"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    static String[] letters = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
     public List<String> letterCombinations(String digits) {
         
-        if(digits.isEmpty()) return new ArrayList();
+        if(digits.length()<1)return new ArrayList();
         
-        List<String> result = letterCombin(digits,"");
+        return combinations(digits,"");
         
-        return result;
     }
     
-    public static List<String> letterCombin(String in, String out) {
-    
+    public static List<String> combinations(String in, String out){
+        
         if(in.isEmpty()){
-            List<String> ans = new ArrayList<>();
+            
+            ArrayList<String> ans = new ArrayList();
             ans.add(out);
-            return ans;
+            return ans;           
         }
-
-        char c = in.charAt(0);
-        int n = c-'0'-1;
         
-        String keys = letters[n];
+        int c = in.charAt(0) - '0';        
+        String letter = letters[c];
+        ArrayList<String> letterCombs = new ArrayList();
         
-        List<String> combinations = new ArrayList<>();
-
-        for(int i=0; i<keys.length(); i++){
-            char letter = keys.charAt(i);
-            combinations.addAll(letterCombin(in.substring(1), out+letter));
+        for(int i=0;i<letter.length();i++){
+            
+            letterCombs.addAll(combinations(in.substring(1),out+letter.charAt(i)));
         }
-
-        return combinations;
-
+        
+        
+        return letterCombs;
     }
+    
     
 }
