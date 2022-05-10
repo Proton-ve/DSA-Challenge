cd2f3738fe99f10352e2bd6f243449556d4e3eef
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-05-10 22:04:39 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (85.67%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    static int[] nums = {1,2,3,4,5,6,7,8,9};
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+     
+        
+        
+        ArrayList<List<Integer>> ans = new ArrayList();
+
+        
+        subset(k, 0, n, new ArrayList(), n, ans);
+        
+        
+        return ans;
+    }
+    
+    
+    public static void subset(int k, int i, int n, List<Integer> out, int sum, ArrayList<List<Integer>> ans){
+
+        
+        // System.out.println(sum+" i si "+i+" out ele are"+out);
+
+        if(i==n-1 || out.size()==k){
+            
+
+            if(sum==0 && out.size()==k){
+                List<Integer> temp = new ArrayList(out);
+                ans.add(temp);
+                return ;
+            }
+            
+            // if(out.size()>0)out.remove(out.size()-1);
+            
+            return ;
+        }
+
+        if(sum<0 || i>n-1 || i>8)return;
+
+        
+        
+        if(sum>0){
+            
+            out.add(nums[i]);            
+            subset(k, i+1, n, out, sum-nums[i], ans);
+            out.remove(out.size()-1);
+            // output.addAll(subset(k, i+1, n, out, sum-nums[i]));
+            
+        }
+            
+      // output.addAll(subset(k,i+1, n, out, sum));
+        
+subset(k,i+1, n, out, sum, ans);
+        return ;
+    }
+}
