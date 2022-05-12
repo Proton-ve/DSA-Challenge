aab7c5a31c71b702d088907185dfa391413b0618
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-05-12 18:48:44 +0530

    Time: 1359 ms (5.00%), Space: 54.6 MB (10.41%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList();        
+        permutaions(nums, new ArrayList(), 0 , result);
+        return result;
+        
+    }
+    
+    
+    public void permutaions(int[] nums, ArrayList<Integer> out, int i, List<List<Integer>> result){
+        
+        
+        if(i>nums.length-1){
+            ArrayList<Integer> ans = new ArrayList(out);
+            if(!result.contains(ans))result.add(ans);
+            return;
+        }
+        
+                
+        for(int j=0; j<=out.size(); j++){
+
+            out.add(j,nums[i]);
+            i++;
+            permutaions(nums, out, i, result);
+            out.remove(j);
+            i--;
+        }
+        
+     return;   
+    }
+}
