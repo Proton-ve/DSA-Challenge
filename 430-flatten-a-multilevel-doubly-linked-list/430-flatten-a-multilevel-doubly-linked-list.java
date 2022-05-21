b04130d8a6b212374ca49fcddfb4448456ecb6c1
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-05-21 19:44:01 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (10.96%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -14,57 +14,42 @@ class Solution {
         if(head==null) return head;
         
         Node temp = head, tail = head;
-        Stack<Node> stack = new Stack();
+        Stack<Node> stack = new Stack();        // to store pointer of next nodes
         
         while(temp.next!=null || temp.child!=null){
             
-            // System.out.println(temp.val);
             
-            if(temp.child!=null){
-                // System.out.println("child "+temp.child.val);
+            if(temp.child!=null){               // if node has a next and child both, then put next node in stack ...
                 if(temp.next!=null)stack.push(temp.next);
-                temp.next = temp.child;
+                temp.next = temp.child;         // and link next to child node  
                 temp.next.prev = temp;
                 temp.child = null;
                 
             }
                 
-            temp = temp.next;
-             
-            tail = temp;
+            temp = temp.next;   // keep moving to next
+            tail = temp;        // keep last node ref, as we have to joint next nodes further 
             
         }
         
         
-        if(stack.isEmpty()){
-         // System.out.println("in empty stk "+head.val);
-            return head;
-        }
+        if(stack.isEmpty()) return head;       // if stack is null, means list is already flatten, then return 
+        
             
-        while(!stack.isEmpty()){
+        while(!stack.isEmpty()){        // if not already flatten, then by this step we will be having all next node pointers in stack, with latest next node on top. Now we will flatten each next node and joint with tail node
             
-            Node cont =  flatten(stack.pop());
-            // System.out.println("check cont "+cont.val+"and tail is "+tail.val);
-            tail.next = cont;
-            cont.prev = tail;
+            Node joint =  flatten(stack.pop()); // flattening next nodes from stack
+            tail.next = joint;
+            joint.prev = tail;
             
-            while(cont.next!=null)cont = cont.next;
+            while(joint.next!=null)joint = joint.next;      // move to tail of joined next node, so that we can link it to upcoming next node in stack
             
-            tail = cont;
+            tail = joint;
             
         }
         
-        
-//         while(head.next!=null){
-            
-//             System.out.println("node val "+head.val);
-            
-//             if(head.child!=null) System.out.println("child val "+head.child.val);
+        // when stack will get empty, means we have flattened all next node and merge with final list
 
-//             head = head.next;
-//         }
-        
-        
         return head;
     }
 }
