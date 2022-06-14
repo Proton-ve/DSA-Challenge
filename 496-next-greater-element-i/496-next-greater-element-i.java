10fe4e44b817fd0a4acf921be7901fb35e8f19b8
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-06-14 20:34:46 +0530

    Time: 3 ms (92.57%), Space: 41.9 MB (99.84%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,58 +1,55 @@
 class Solution {
-//     public int[] nextGreaterElement(int[] nums1, int[] arr) {
+    
+    public int[] nextGreaterElement(int[] nums1, int[] arr) {
         
-//         int n = arr.length-1;
-     
-//         Stack<Integer> stack = new Stack<>();
-//         Map<Integer,Integer> nextGreaterEle = new HashMap();
+        int n = arr.length-1;
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer,Integer> nextGreaterEle = new HashMap();
         
-//         for(int i=n;i>=0;i--){
+        for(int i=n;i>=0;i--){
 
-//             while(!stack.isEmpty() && stack.peek()<arr[i]) stack.pop();
+            while(!stack.isEmpty() && stack.peek()<arr[i])
+                stack.pop();
             
-//             nextGreaterEle.put(arr[i],(stack.isEmpty() ? -1:stack.peek()));
-            
-//             stack.push(arr[i]);
-
-//         }
+            nextGreaterEle.put(arr[i],(stack.isEmpty() ? -1:stack.peek()));
+            stack.push(arr[i]);
+        }
         
-//         int n1 = nums1.length;
+        int n1 = nums1.length;
         
-//         for(int i=0; i<n1; i++) nums1[i] = nextGreaterEle.get(nums1[i]);
-         
-
-//         return nums1;
-//     }
+        for(int i=0; i<n1; i++)
+            nums1[i] = nextGreaterEle.get(nums1[i]);
+        
+        return nums1;
+    }
     
     
     // brute force
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int len1 = nums1.length;
-        int len2 = nums2.length;
+//         int len1 = nums1.length;
+//         int len2 = nums2.length;
         
-        Map<Integer,Integer> nextGreaterEle = new HashMap();
+//         Map<Integer,Integer> nextGreaterEle = new HashMap();
         
-        for(int i=0; i<len2;i++){
+//         for(int i=0; i<len2;i++){
             
-            nextGreaterEle.put(nums2[i],-1);
+//             nextGreaterEle.put(nums2[i],-1);
                         
-            for(int j=i+1; j<len2; j++){
+//             for(int j=i+1; j<len2; j++){
                                 
-                if(nums2[j]>nums2[i]){
+//                 if(nums2[j]>nums2[i]){
                     
-                    nextGreaterEle.put(nums2[i],nums2[j]);
-                    break;
-                    
-                }
-            }
-            
-        }
+//                     nextGreaterEle.put(nums2[i],nums2[j]);
+//                     break;
+//                 }
+//             }
+//         }
                 
-        for(int i=0; i<len1; i++) nums1[i] = nextGreaterEle.get(nums1[i]);
+//         for(int i=0; i<len1; i++) nums1[i] = nextGreaterEle.get(nums1[i]);
      
-     return nums1;  
-    }
+//      return nums1;  
+//     }
     
     
     
