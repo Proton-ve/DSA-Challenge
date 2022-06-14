cf73a45758eca3a54929e6defa4bc8f7bcb5c49e
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-06-14 09:08:40 +0530

    Time: 1 ms (54.68%), Space: 45.8 MB (35.69%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,96 +9,38 @@
  * }
  */
 class Solution {
-    
-    
-    public boolean check(ListNode head, int k){
-        
-        while(head!=null && k>0){
-            
-            head = head.next;
-            k--;
-            
-        }
-        
-        return k==0;
-        
-    }
-    
-    
-    
     public ListNode reverseKGroup(ListNode head, int k) {
         
-        if(k==1) return head;
-        int t = k-1;
-        ListNode temp = head;
-        ListNode dummy = head;
+        ListNode pointer = new ListNode(0,head);
+        ListNode ans = pointer;
         
-        while(t>0){
+        while(pointer!=null){
             
-            temp = temp.next;
-            t--;
-        }
-        
-        int left = 1, right = k;
-        ListNode temp2 = head;
-        
-        while(check(temp2,k)){
- 
-            temp2 = reverseBetween(dummy,left,right, k);
-            dummy = temp;
-            left = left+k;
-            right = right+k;
+            ListNode testNode = pointer.next;
             
-        }
+            for(int i=0;i<k-1 && testNode!=null;i++){
+                testNode = testNode.next;                
+            }
+            
+            if(testNode==null)break;
+            
+            ListNode prev = pointer, cur = pointer.next, next = null;
+            
+            for(int i=0;i<k;i++){
                 
-        return temp;
-        
-    }
-    
-    
-    
-    public ListNode reverseBetween(ListNode head, int left, int right, int k) {
-
-        
-        if(head==null || head.next==null || left==right)return head;
-        
-        ListNode cur = head;
-        ListNode cut1 = null;
-        int count = right - left;
-        int temp = left;
-        
-        while(cur!=null && left>1){
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
             
-            cut1 = cur;
-            cur = cur.next;
-            left--;
-                        
-        }
-  
-        ListNode cut2 = cur;
-        
-        ListNode prev = null;
-        ListNode second = null;
-        
-        
-        while(cur!=null && count>=0){
-                        
-            second = cur.next!=null ? cur.next : null;
-            cur.next = prev;
-            prev = cur;
-            cur = second;
-            count--;
-           
+            ListNode first = pointer.next;
+            first.next = cur;
+            pointer.next = prev;
+            pointer = first;
+            
         }
         
-        
-        if(cut1!=null)cut1.next = prev;
-        else head = prev;
-        
-        cut2.next = cur;
-                
-        return cut2.next;
+        return ans.next;
     }
-    
-
 }
