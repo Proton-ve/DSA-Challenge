473ec19916356ede18cff2738a63fe1acc023d76
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-06-16 20:18:33 +0530

    Time: 5 ms (63.00%), Space: 42.4 MB (81.85%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -3,37 +3,24 @@ class Solution {
         
         int n = s.length(), ans = 0;
         
-        Stack<Character> para = new Stack();
         Stack<Integer> index = new Stack();
         
-        index.push(-1);
+        index.push(-1); // -1 becoz If 1st char is ')', then we will get stack empty error without '-1' at line(21)
         
         for(int i=0; i<n; i++){
             
             char c = s.charAt(i);
             
             if(c=='('){
-                
-                index.push(i);
-                para.push('(');
-                
+                index.push(i);                
             }else{
-                
-                if(!para.isEmpty()){
-                    
-                    para.pop();
-                    index.pop();
+                index.pop();
+                if(!index.isEmpty()){
                     ans = Math.max(i-index.peek(),ans);
-                    
-                    
                 }else{
-                    
                     index.push(i);
-                    
                 }
-                
             }
-            
         }
     
         return ans;
