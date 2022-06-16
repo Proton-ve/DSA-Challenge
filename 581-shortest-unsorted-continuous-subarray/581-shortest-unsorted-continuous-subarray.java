609b06fba983f8d092f32aacbd358fc9c24d76f3
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-06-16 21:15:49 +0530

    Time: 2 ms (83.93%), Space: 54.4 MB (29.44%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -5,20 +5,31 @@ class Solution {
         int n = nums.length;
         int end = -1, begin = 0;
         
-        for(int i=n-1;i>=0;i--){
-            
-            min = Math.min(min,nums[i]);
-            if(nums[i]>min){
-                begin=i;
-            }
-        }
         
         for(int i=0;i<n;i++){
-            
+            int rev = n-i-1;
             max = Math.max(max,nums[i]);
+            min = Math.min(min,nums[rev]);
             if(nums[i]<max)
                 end = i;
+            if(nums[rev]>min)
+                begin = rev;
         }
+        
+//         for(int i=n-1;i>=0;i--){
+            
+//             min = Math.min(min,nums[i]);
+//             if(nums[i]>min){
+//                 begin=i;
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++){
+            
+//             max = Math.max(max,nums[i]);
+//             if(nums[i]<max)
+//                 end = i;
+//         }
         
         return end-begin +1;
     }
