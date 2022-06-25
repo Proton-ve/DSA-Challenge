af6b3bfca8eb51a746e8eefd9f3d1f637fa9348d
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-06-25 22:06:04 +0530

    Time: 5 ms (55.53%), Space: 43.2 MB (36.05%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,99 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    class Tuple{
+        
+        int val, col, row;
+        TreeNode node;
+        
+        public Tuple(int val, int row, int col, TreeNode node){
+            this.val = val;
+            this.row = row;
+            this.col = col;
+            this.node = node;
+        }
+        
+    }
+    
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        
+        TreeMap<Integer, TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap();
+        
+        Queue<Tuple> q = new ArrayDeque();
+        q.offer(new Tuple(root.val,0,0,root));
+        
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            for(int i=0;i<size;i++){
+                
+                Tuple temp = q.poll();
+                int row = temp.row, col = temp.col;
+
+                if(!map.containsKey(col)){
+                    map.put(col, new TreeMap());                
+                }
+
+                if(!map.get(col).containsKey(row)){
+                    map.get(col).put(row,new PriorityQueue<Integer>());
+                }
+
+                map.get(col).get(row).offer(temp.val);
+
+                TreeNode node = temp.node;
+
+                if(node.left!=null){
+                    q.offer(new Tuple(node.left.val,row+1, col-1, node.left));
+                }
+
+                if(node.right!=null){
+                    q.offer(new Tuple(node.right.val,row+1, col+1, node.right));
+                }
+            }
+        }
+
+        List<List<Integer>> list = new ArrayList();
+        
+//                     System.out.println("map vals are");
+
+//                     System.out.println(map.values());
+
+//         System.out.println("q vals are");
+
+        for(TreeMap<Integer,PriorityQueue<Integer>> e : map.values()){
+            
+            // List<Integer> level = new ArrayList();
+            list.add(new ArrayList());
+            
+            // System.out.println(e.values());
+            
+            for(PriorityQueue<Integer> f : e.values()){
+                
+                while(!f.isEmpty()){
+                    list.get(list.size()-1).add(f.poll());
+                    // level.add(f.poll());
+                }
+                // list.add(level);
+            }
+            
+        }
+        
+        return list;
+    }
+}
