94709aba4cb5e6aaadf4fb0f77d98e85abd2c5a3
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-07-02 13:48:23 +0530

    Added solution - LeetHub

--- Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.java
+++ Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.java
@@ -36,23 +36,49 @@ class Solution{
     static long maximumSumSubarray(int k, ArrayList<Integer> Arr,int n){
         // code here
         
-        int i=0;
-        long sum=0;
+        int i=0,j=0;
+        long ans=-1,sum=0;
         
-        for(i=0;i<k;i++){
-            sum+=Arr.get(i);
-        }
-        
-        int start=0, end=i;
-        long ans=sum;
-        
-        while(end<n){
+        while(j<n){
             
-            sum+=Arr.get(end++);
-            sum-=Arr.get(start++);
-            ans = Math.max(sum,ans);
+            sum+=Arr.get(j);
+            
+            if((j-i+1)<k){
+                j++;
+            }
+            else
+                if((j-i+1)==k){
+                    ans = Math.max(sum,ans);
+                    sum-=Arr.get(i);
+                    j++;
+                    i++;
+                }
         }
         
         return ans;
+        
+        
+        
+        // method 1 :
+        
+        // int i=0;
+        // long sum=0;
+        
+        // for(i=0;i<k;i++){
+        //     sum+=Arr.get(i);
+        // }
+        
+        // int start=0, end=i;
+        // long ans=sum;
+        
+        // while(end<n){
+            
+        //     sum+=Arr.get(end++);
+        //     sum-=Arr.get(start++);
+        //     ans = Math.max(sum,ans);
+        // }
+        
+        // return ans;
+        
     }
 }
