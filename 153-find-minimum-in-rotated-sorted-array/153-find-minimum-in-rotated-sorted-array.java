99f292b9acc188d362b39a4cc9c5ddead56df1cc
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-07-15 20:50:13 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (6.32%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -10,11 +10,20 @@ class Solution {
             int mid = start+(end-start)/2;
             
             int midEle = nums[mid];
-            int prev = (mid-1+n)%n;
-            int next = (mid+1)%n;
+            // int prev = (mid-1+n)%n; // If mid is 0, then doing simply mid-1 will give indexoutofbound err, and mid will be 0 only if                                            array is sorted
+            // int next = (mid+1)%n;
+            
+            int prev = mid>0 ? mid-1 : n-1;
+            int next = mid<n-1 ? mid+1 : 0;
             
             if(midEle<=nums[prev] && midEle<=nums[next])
                 return midEle;
+            
+            /**
+            * Logic :::
+            * Keep searching in unsorted part & eliminate sorted part
+            * If both the part is sorted, then (keep searching in rightest part) min element shall exist in rightest part of array, as             * array is sorted.
+            */
             
             if(midEle<nums[start])
                 end = mid-1;
@@ -22,8 +31,8 @@ class Solution {
                 start = mid+1;
             else 
                 end = mid-1;
-                        
         }
+        
         return -1;
     }
 }
