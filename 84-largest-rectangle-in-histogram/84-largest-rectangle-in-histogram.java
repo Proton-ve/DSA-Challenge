dc9b5ec4be368cc7739ec613623bff742a3658a5
Raj Manjrekar <79650580+Raj-m01@users.noreply.github.com>
2022-09-16 21:34:29 +0530

    Time: 216 ms (40.88%), Space: 90.3 MB (54.38%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,66 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         
-        int[] prevSmallers = previousSmaller(heights);
-        int[] nextSmallers = nextSmaller(heights);
-                
-        int n = heights.length;
-        int maxArea = 0;
+        int maxArea=0, n=heights.length;
+        Stack<Integer> stack = new Stack();
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             
-            int currArea = (nextSmallers[i] - prevSmallers[i]-1)*heights[i];
+            int cur = i<n?heights[i]:0;
             
-            if(currArea>maxArea)maxArea = currArea;
-                    
-        }
+            if(!stack.isEmpty() && heights[stack.peek()]<=cur){
+                stack.push(i);
+            }else{
                 
+                while(!stack.isEmpty() && heights[stack.peek()]>cur){
+                    
+                    int curHeight = stack.pop();
+                    int curArea = stack.isEmpty()?heights[curHeight]*(i-0)
+                                                 :heights[curHeight]*(i-stack.peek()-1);
+                    maxArea = Math.max(curArea,maxArea);
+                }
+                stack.push(i);
+            }
+        }
         return maxArea;
     }
-    
-    
-    
-    public int[] previousSmaller(int[] arr){
-        
-        int n = arr.length;
-        int[] ans = new int[n];
-        
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i=0; i<n;i++){
-
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]) stack.pop();
-
-            ans[i] = stack.isEmpty() ? -1:stack.peek();
-
-            stack.push(i);
-
-        }
-     
-        return ans;
-    }
-    
-    
-    public int[] nextSmaller(int[] arr){
-        
-        int n = arr.length;
-        int[] ans = new int[n];
-        
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i=n-1;i>=0;i--){
-
-            while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]) stack.pop();
-
-            ans[i] = stack.isEmpty() ? n:stack.peek();
-
-            stack.push(i);
-
-
-        }
-        
-        return ans;
-    }
-    
-    
 }
